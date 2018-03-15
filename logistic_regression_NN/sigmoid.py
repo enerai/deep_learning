ffd15a49cc49517bfb053c6be794ad5a5a20cad8
Ankedionfr <sunqihang@qq.com>
2018-03-15 23:32:49 +0800

    Create sigmoid.py

---
+++ logistic_regression_NN/sigmoid.py
@@ -0,0 +1,14 @@
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    s = 1/(1+np.exp(-z))
+    
+    return s
