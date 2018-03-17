948166c97f4f3367081cc19aea6f06dfad74ad88
Ankedionfr <sunqihang@qq.com>
2018-03-17 17:15:06 +0800

    Create compute_cost.py

---
+++ neural_networks/compute_cost.py
@@ -0,0 +1,26 @@
+def compute_cost(A2, Y, parameters):
+    """
+    Computes the cross-entropy cost (objective value of logistic regression)
+    
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+    
+    Returns:
+    cost -- cross-entropy cost (objective value of logistic regression)
+    """
+    
+    m = Y.shape[1] # number of example
+
+
+
+    logprobs = np.multiply(np.log(A2),Y)+np.multiply(np.log(1-A2),1-Y)
+    cost = - np.sum(logprobs)/m
+
+    
+    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
+                                # E.g., turns [[17]] into 17 
+    assert(isinstance(cost, float))
+    
+    return cost
