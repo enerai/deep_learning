21ff61479c52d0154baf5814acdfcb2b851cda0c
Enerai <sunqihang@qq.com>
2018-03-26 18:35:46 +0800

    Create create_placeholders.py

---
+++ signs_recognition/create_placeholders.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from cnn_utils import *
+
+def create_placeholders(n_H0, n_W0, n_C0, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_H0 -- scalar, height of an input image
+    n_W0 -- scalar, width of an input image
+    n_C0 -- scalar, number of channels of the input
+    n_y -- scalar, number of classes
+        
+    Returns:
+    X -- placeholder for the data input, of shape [None, n_H0, n_W0, n_C0] and dtype "float"
+    Y -- placeholder for the input labels, of shape [None, n_y] and dtype "float"
+    """
+
+    X = tf.placeholder(tf.float32,[None, n_H0, n_W0, n_C0])
+    Y = tf.placeholder(tf.float32,[None, n_y])
+
+    
+    return X, Y
